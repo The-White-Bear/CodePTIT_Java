d092810af81ca38149c3039e7829e481ed59a601
Duythong <Duythong.ptit@gmail.com>
2023-09-06 00:17:20 +0700

    Add file J02017 - Thu gon day so

---
+++ J02017.java
@@ -0,0 +1,31 @@
+package CodePTIT_Java;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class J02017{
+
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int n = ip.nextInt();
+        List<Integer> a = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            a.add(ip.nextInt());
+        }
+        while (true) {
+            boolean stop = true;
+            for (int i = 0; i < a.size() - 1; i++) {
+                if ((a.get(i) + a.get(i + 1)) % 2 == 0) {
+                    a.remove(i + 1);
+                    a.remove(i);
+                    i--;
+                    stop = false;
+                }
+            }
+            if (stop) {
+                System.out.println(a.size());
+                return;
+            }
+        }
+    }
+}
