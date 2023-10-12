7f45d5afb9d95d7881ae08940e4f28f9a9118488
Duythong <Duythong.ptit@gmail.com>
2023-10-12 10:59:05 +0700

    Add file

---
+++ J07018.java
@@ -0,0 +1,48 @@
+package CodePTIT_Java;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Pair<T,U> {
+    T first;
+    U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+    @Override
+    public String toString() {
+        return "("+ first + "," + second + ")";
+    }
+}
+public class J07018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[][] adj = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                adj[i][j] = scanner.nextInt();
+            }
+        }
+
+        List<Pair<Integer, Integer>> edgeList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (adj[i][j] == 1) {
+
+                    edgeList.add(new Pair<>(i+1, j+1));
+                }
+            }
+        }
+
+
+        for (Pair<Integer, Integer> edge : edgeList) {
+            System.out.println(edge);
+        }
+    }
+}
